d9bc6358b3936ddc3c747285af31330369a15bc2
Hugo Lundin <hugo.lundin@hotmail.com>
2016-04-06 10:46:24 +0200

    Fixed some linting errors.

--- web/app/models.py
+++ web/app/models.py
@@ -1,4 +1,6 @@
 from datetime import datetime
 from . import db
 
-# Add database model, see example in https://github.com/miguelgrinberg/flasky/blob/master/app/models.py
+"""Add database model, see example in
+https://github.com/miguelgrinberg/flasky/blob/master/app/models.py
+"""
