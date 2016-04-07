b8f7692c71fe75bc5114d7cffa420df156e58c04
Hugo Lundin <hugo.lundin@hotmail.com>
2016-04-07 11:44:51 +0200

    Added auth/views.py-imports.

---
+++ web/app/auth/views.py
@@ -0,0 +1,8 @@
+from flask import render_template, redirect, request, url_for, flash
+from flask.ext.login import login_user, logout_user, login_required, \
+    current_user
+from . import auth
+from .. import db
+from ..models import User
+from ..email import send_email
+from .forms import 
