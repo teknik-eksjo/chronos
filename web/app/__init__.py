9e0eded268788a7c0bf69b0acaffa5c1325c7df6
Hugo Lundin <hugo.lundin@hotmail.com>
2016-04-07 10:27:49 +0200

    Added auth blueprint and imported LoginManager.

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.login import LoginManager
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
@@ -8,6 +9,10 @@ from config import config
 bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -25,5 +30,8 @@ def create_app(config_name):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
