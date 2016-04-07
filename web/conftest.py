c0c2994a13baf83b2b6c82fb04eceed0eac24242
Linus Törngren <linus@etnolit.se>
2016-04-07 11:30:04 +0200

    Improved manage.py
    By default it now ignores tests that use the live_server fixture (Selenium).

--- web/conftest.py
+++ web/conftest.py
@@ -1,6 +1,24 @@
 import pytest
 from app import create_app
 
+
+def pytest_collection_modifyitems(items):
+    """Handle custom markers.
+
+    pytest hool called after test collection has been performed.
+
+    Adds a marker named "gui" to enable filtering of tests.
+
+    Args:
+        items: list of _pytest.main.Node items, each item represents a test
+
+    Reference:
+        http://pytest.org/latest/plugins.html
+    """
+    for item in items:
+        if 'live_server' in getattr(item, 'fixturenames', ()):
+            item.add_marker('gui')
+
 @pytest.fixture
 def app():
     app = create_app('testing')
