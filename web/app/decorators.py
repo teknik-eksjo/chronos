61585cd2cbfd677e631580897dc18d355a6f9671
Hugo Lundin <hugo.lundin@hotmail.com>
2016-04-07 10:28:45 +0200

    Added decorator for required permissions, and added exception-class for ValidationError

---
+++ web/app/decorators.py
@@ -0,0 +1,14 @@
+from functools import wraps
+from flask import abort
+from flask.ext.login import current_user
+
+
+def permission_required(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.can(permission):
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
