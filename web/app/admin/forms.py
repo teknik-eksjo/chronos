d056d1e9457ed39abf7b682310925776f697cc3b
Hugo Lundin <hugo.lundin@hotmail.com>
2016-04-26 11:46:46 +0200

    Added a valid regular expression for validation of first- and last name.

--- web/app/admin/forms.py
+++ web/app/admin/forms.py
@@ -4,21 +4,20 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.validators import Required, Length, Email, EqualTo, Regexp
 from wtforms import ValidationError
 from ..models import User
+import re
 
 
 class AddTeacherForm(Form):
     first_name = StringField('Förnamn', validators=[
-        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-                                          'Usernames must have only letters, '
-                                          'numbers, dots or underscores')])
+        Required(), Length(1, 64), Regexp('^[A-Ö]*(-| )?[A-Ö]*(-| )?[A-Ö]*$', re.IGNORECASE,
+                                          'Förnamn får endast innehålla A-Ö, a-ö, mellanslag och -.')])
     last_name = StringField('Efternamn', validators=[
-        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-                                          'Usernames must have only letters, '
-                                          'numbers, dots or underscores')])
-    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
-    email2 = StringField('Verfiera Email', validators=[Required(),
+        Required(), Length(1, 64), Regexp('^[A-Ö]*(-| )?[A-Ö]*(-| )?[A-Ö]*$', re.IGNORECASE,
+                                          'Efternamn får endast innehålla A-Ö, a-ö, mellanslag och -.')])
+    email = StringField('Epost', validators=[Required(), Length(1, 64), Email()])
+    email2 = StringField('Verfiera Epost', validators=[Required(),
                                                        EqualTo('email',
-                                                               message='Email måste vara samma i båda fälten.'),
+                                                               message='Epost måste vara samma i båda fält.'),
                                                        Email()])
     submit = SubmitField('Lägg till')
 
