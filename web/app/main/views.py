f92cd1153303fef6ab8213c7f24ad3d7d696b28b
Szymon Stypa <shabo.o@hotmail.com>
2016-04-27 12:53:49 +0200

    Created a route for creating base schedules. Modified the slider and included it in the corresponding template, which along with index.html was moved to a 'main' template folder. Minor css changes in the theme and a new 'style.css' file which is to contain general styling - not bootstrap theme or slider related.

--- web/app/main/views.py
+++ web/app/main/views.py
@@ -1,10 +1,16 @@
 from . import main
 from .. import db
 from flask import render_template, url_for, redirect
+from flask.ext.login import login_required
 # from ..models import Model
 # from .forms import Form
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('main/index.html')
+
+@main.route('/base-schedule', methods=['GET', 'POST'])
+@login_required
+def base_schedule():
+    return render_template('main/base_schedule.html')
