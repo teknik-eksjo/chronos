3b5695b45e2e264ecc35831749a90fb4bf8e6a54
Szymon Stypa <shabo.o@hotmail.com>
2016-04-27 14:18:53 +0200

    Created a route for creating base schedules (#79)
    * Created a route for creating base schedules. Modified the slider and included it in the corresponding template, which along with index.html was moved to a 'main' template folder. Minor css changes in the theme and a new 'style.css' file which is to contain general styling - not bootstrap theme or slider related.
    
    * Fixed linting error in main/views.py 13:1

---
+++ web/app/static/js/create-slider.js
@@ -0,0 +1,62 @@
+$(function() {
+
+  // create slider with an options object that will override the defaut config.
+  options = {
+    min: 360,
+    max: 1080,
+    step: 5,
+    tooltips: true,
+    handles: [{
+      value: 480,
+      type: "wake"
+    }, {
+      value: 690,
+      type: "leave"
+    }, {
+      value: 750,
+      type: "return"
+    }, {
+      value: 960,
+      type: "sleep"
+    }],
+    showTypeNames: true,
+    typeNames: {
+      'wake': 'Start',
+      'leave': 'Lunch',
+      'return': 'Lunch slut',
+      'sleep': 'Slut'
+    },
+    mainClass: 'sleep',   // Main css class (default background style of slider)
+    type: 'time',
+    slide: function(e, ui) {
+
+      var index = ui.values.indexOf(ui.value)
+
+      // Work persiods no longer than 5h
+      if (ui.values[1] - ui.values[0] > 300) {
+        $(jQuery(this).children('.ui-slider-range.wake')).addClass('errorbg')
+      } else {
+        $(jQuery(this).children('.ui-slider-range.wake')).removeClass('errorbg')
+      }
+
+      // Lunch break must be at least 30 minutes
+      if (ui.values[2] - ui.values[1] < 30) {
+        $(jQuery(this).children('.ui-slider-range.leave')).addClass('errorbg')
+      } else {
+        $(jQuery(this).children('.ui-slider-range.leave')).removeClass('errorbg')
+      }
+
+      // Work period 2 can't be longer than 5 hours
+      if (ui.values[3] - ui.values[2] > 300) {
+        $(jQuery(this).children('.ui-slider-range.return')).addClass('errorbg')
+      } else {
+        $(jQuery(this).children('.ui-slider-range.return')).removeClass('errorbg')
+      }
+    },
+    change: function(e, ui) {
+      //TODO: send updated info to server here.
+    }
+  }
+
+  $('.period-slider').slider(options);
+});
