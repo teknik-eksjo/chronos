d3c42dbcf524d12f55d586ef519ea75746039497
Linus Törngren <linus@etnolit.se>
2016-04-28 22:20:08 +0200

    Added fixtures for database session.
    When writing tests that need the session object use `session` as argument. The fixture will then make sure to clean the database between tests.
    
    closes #82

--- web/tests/test_user_model.py
+++ web/tests/test_user_model.py
@@ -1,5 +1,4 @@
 import pytest
-from app import db
 from app.models import User
 
 def test_password_setter():
@@ -21,31 +20,31 @@ def test_password_salts_are_random():
     u2 = User(password='cat')
     assert u.password_hash != u2.password_hash
 
-def test_valid_email_change():
+def test_valid_email_change(session):
     u = User(email='test@example.com')
-    db.session.add(u)
-    db.session.commit()
+    session.add(u)
+    session.commit()
 
     token = u.generate_email_change_token('new-email@example.com')
     assert u.change_email(token)
 
-def test_duplicate_email_change():
+def test_duplicate_email_change(session):
     u = User(email='test@example.com')
     u2 = User(email='old-email@example.com')
-    db.session.add(u)
-    db.session.add(u2)
-    db.session.commit()
+    session.add(u)
+    session.add(u2)
+    session.commit()
 
     token = u2.generate_email_change_token('test@example.com')
     # It's not possible to change to another user's email
     assert u2.change_email(token) is False
 
-def test_invalid_email_change_token():
+def test_invalid_email_change_token(session):
     u1 = User(email='john@example.com', password='cat')
     u2 = User(email='susan@example.org', password='dog')
-    db.session.add(u1)
-    db.session.add(u2)
-    db.session.commit()
+    session.add(u1)
+    session.add(u2)
+    session.commit()
     token = u1.generate_email_change_token('david@example.net')
     # It's not possible to use another user's token
     assert u2.change_email(token) is False
