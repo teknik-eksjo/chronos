bef426a7ec2c194ee7236ac1c6e10fe460ab443f
Linus Törngren <linus@etnolit.se>
2016-04-28 14:15:29 +0200

    Added fixtures for database session.
    When writing tests that need the session object use `session` as argument. The fixture will then make sure to clean the database between tests.

--- web/tests/conftest.py
+++ web/tests/conftest.py
@@ -1,4 +1,5 @@
 from app import create_app
+from app import db as _db
 import pytest
 
 def pytest_collection_modifyitems(items):
@@ -19,8 +20,41 @@ def pytest_collection_modifyitems(items):
             item.add_marker('gui')
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def app():
     """Create an application context."""
     app = create_app('testing')
     return app
+
+
+@pytest.fixture(scope='session')
+def db(app, request):
+    """Session wide database connection."""
+
+    def teardown():
+        _db.drop_all()
+
+    _db.app = app
+    _db.create_all()
+
+    request.addfinalizer(teardown)
+    return _db
+
+@pytest.fixture(scope='function')
+def session(db, request):
+    """Create a new database session for a test."""
+    connection = db.engine.connect()
+    transaction = connection.begin()
+
+    options = dict(bind=connection, binds={})
+    session = db.create_scoped_session(options=options)
+
+    db.session = session
+
+    def teardown():
+        transaction.rollback()
+        connection.close()
+        session.remove()
+
+    request.addfinalizer(teardown)
+    return session
