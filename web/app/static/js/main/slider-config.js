99548d8c96490ba4b7b87c38b64c1772fa43437c
Szymon Stypa <shabo.o@hotmail.com>
2016-05-11 13:05:01 +0200

    Created most of the front end for the deviations page. This resulted in some fiddling with js and css files for the base-schedule page aswell. You can now add deviations and remove them. The date picker is temporary.

---
+++ web/app/static/js/main/slider-config.js
@@ -0,0 +1,65 @@
+// create slider with an options object that will override the defaut config.
+var options = {
+  min: 360,
+  max: 1080,
+  step: 5,
+  tooltips: true,
+  handles: [{
+    value: 480,
+    type: 'wake'
+  }, {
+    value: 690,
+    type: 'leave'
+  }, {
+    value: 750,
+    type: 'return'
+  }, {
+    value: 960,
+    type: 'sleep'
+  }],
+  showTypeNames: true,
+  typeNames: {
+    'wake': 'Start',
+    'leave': 'Lunch',
+    'return': 'Lunch slut',
+    'sleep': 'Slut'
+  },
+  mainClass: 'sleep',   // Main css class (default background style of slider)
+  type: 'time',
+  slide: function (e, ui) {
+    // Work persiods no longer than 5h
+    if (ui.values[1] - ui.values[0] > 300) {
+      $(jQuery(this).children('.ui-slider-range.wake')).addClass('errorbg')
+    } else {
+      $(jQuery(this).children('.ui-slider-range.wake')).removeClass('errorbg')
+    }
+
+    // Lunch break must be at least 30 minutes
+    if (ui.values[2] - ui.values[1] < 30) {
+      $(jQuery(this).children('.ui-slider-range.leave')).addClass('errorbg')
+    } else {
+      $(jQuery(this).children('.ui-slider-range.leave')).removeClass('errorbg')
+    }
+
+    // Work period 2 can't be longer than 5 hours
+    if (ui.values[3] - ui.values[2] > 300) {
+      $(jQuery(this).children('.ui-slider-range.return')).addClass('errorbg')
+    } else {
+      $(jQuery(this).children('.ui-slider-range.return')).removeClass('errorbg')
+    }
+  },
+  change: function (e, ui) {
+    var data = { 'values': ui.values }
+
+    data.index = $(this).slider('option', 'index')
+
+    $.post({
+      url: window.location.href,
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      success: function () {
+        console.log('Data sent!')
+      }
+    })
+  }
+}
